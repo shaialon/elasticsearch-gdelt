1e40ccbe77ae5350491c40b45a0ed1088d333554
“shaialon” <shaialon84@gmail.com>
2015-11-21 22:35:59 +0200

    Read sample file...

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola world");
+"use strict";
+
+// External dependencies
+const fs = require('fs'),
+      _  = require ('lodash'),
+      transform = require('stream-transform'),
+      parse = require('csv-parse');
+
+
+const indexName = '20151028';
+
+const input = fs.createReadStream(__dirname+'/samples/'+indexName+'.export.CSV');
+const parser = parse({delimiter: '\t'});
+
+input.pipe(process.stdout);
+
