20c404f0bc80725c77de0ac764cc354de557fa8c
“shaialon” <shaialon84@gmail.com>
2015-11-22 00:03:08 +0200

    Geopoints and date

--- gdelt/formatter.js
+++ gdelt/formatter.js
@@ -25,7 +25,7 @@ const formatter = function(record){
             let action = columns[index];
 
             if(discardedFields.has(action)){
-                // This field is discarded from the object going forward (to save overhead)
+                // The field is discarded from the object going forward (to save overhead)
             }
             else if(cameoToHuman[action]){
                 // Turns CAMEO codes into human readable strings
@@ -34,6 +34,20 @@ const formatter = function(record){
             else if (_.startsWith(action ,'Num') || _.startsWith(action ,'Avg') || action === 'GoldsteinScale'){
                 // Turns numbers from strings to actual numbers that can be evaluated , sorted , aggregated...
                 newRecord[action]=Number(columnValue);
+            }
+            else if(_.endsWith(action ,'Geo_FullName')){
+                // Transform "Petersburg, Sankt-Peterburg, Russia" into ["Petersburg", "Sankt-Peterburg", "Russia"] for easy evaluation in index
+                newRecord[action]=columnValue.split(", ");
+            }
+            else if(_.endsWith(action ,'Geo_Lat') || _.endsWith(action ,'Geo_Long')){
+                // Transform "Actor1Geo_Long":"70", "Actor1Geo_Lat":"30" in elastichsearch geo points Actor1Geo_Location = {lat:30,lon:70}
+                let newAction = `${action.split('_')[0]}_Location`;
+                if(!newRecord[newAction]){newRecord[newAction] = {};}
+                newRecord[newAction][action.substr(10,3).toLowerCase()] = Number(columnValue);
+            }
+            else if (action === 'DATEADDED'){
+                // Transform Date into elasticsearch format
+                newRecord[action]=`${columnValue.substr(0,4)}-${columnValue.substr(4,2)}-${columnValue.substr(6,2)}`;
             }
             else {
                 // Default : just the string value
